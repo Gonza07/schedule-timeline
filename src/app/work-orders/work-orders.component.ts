bfe4fffb12f8cf395cc61e81c9319551864780da
Gonzalo Laborde <gonzalolaborde07@gmail.com>
2025-12-29 01:58:01 -0300

    Fix ng-select styling. Starting work center section

--- src/app/work-orders/work-orders.component.ts
+++ src/app/work-orders/work-orders.component.ts
@@ -1,19 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-work-orders',
   standalone: true,
-  imports: [NgSelectModule],
+  imports: [CommonModule,NgSelectModule],
   templateUrl: './work-orders.component.html',
   styleUrl: './work-orders.component.scss',
 })
 export class WorkOrdersComponent {
-  selectedTimescale: string = '';
-  timescales = [
+  public selectedTimescale: string = '';
+  public timescales = [
     { value: 'hour', label: 'Hour' },
     { value: 'day', label: 'Day' },
     { value: 'week', label: 'Week' },
     { value: 'month', label: 'Month' },
+  ];
+
+  public workCenters = [
+    { id: 1,value: 'genesisHardware', label: 'Genesis Hardware' },
+    { id: 2,value: 'rodriguesElectrics', label: 'Rodrigues Electrics' },
+    { id: 3,value: 'konsultingInc', label: 'Konsulting Inc' },
+    { id: 4,value: 'mcMarrowDistribution', label: 'McMarrow Distribution' },
+    { id: 5,value: 'spartanManufacturing', label: 'Spartan Manufacturing' },
   ];
 }
