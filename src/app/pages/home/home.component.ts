cd05da1f443b6504f24a0a637ea3e25cb506e380
Gonzalo Laborde <gonzalolaborde07@gmail.com>
2025-12-29 01:38:07 -0300

    Starting with work orders component. Bootstrap and ng-select installed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TimelineScheduleComponent } from '../../timeline-schedule/timeline-schedule.component';
+import { WorkOrdersComponent } from '../../work-orders/work-orders.component';
+import { WorkOrderDetailsComponent } from '../../work-order-details/work-order-details.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [WorkOrdersComponent,TimelineScheduleComponent, WorkOrderDetailsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/work-orders/work-orders.component.ts
+++ src/app/work-orders/work-orders.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-work-orders',
   standalone: true,
-  imports: [],
+  imports: [NgSelectModule],
   templateUrl: './work-orders.component.html',
-  styleUrl: './work-orders.component.scss'
+  styleUrl: './work-orders.component.scss',
 })
 export class WorkOrdersComponent {
-
+  selectedTimescale: string = '';
+  timescales = [
+    { value: 'hour', label: 'Hour' },
+    { value: 'day', label: 'Day' },
+    { value: 'week', label: 'Week' },
+    { value: 'month', label: 'Month' },
+  ];
 }
