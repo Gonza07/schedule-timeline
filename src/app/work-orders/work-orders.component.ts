ceb3151a34e5775ac99822b8c3cf4fad5b4cbe2b
Gonzalo Laborde <gonzalolaborde07@gmail.com>
2025-12-29 16:43:40 -0300

    Format code in work orders component. Add default value to timescale select

--- src/app/work-orders/work-orders.component.ts
+++ src/app/work-orders/work-orders.component.ts
@@ -2,16 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { TimelineScheduleComponent } from '../timeline-schedule/timeline-schedule.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-work-orders',
   standalone: true,
-  imports: [CommonModule,NgSelectModule, TimelineScheduleComponent],
+  imports: [CommonModule,NgSelectModule, TimelineScheduleComponent, FormsModule],
   templateUrl: './work-orders.component.html',
   styleUrl: './work-orders.component.scss',
 })
 export class WorkOrdersComponent {
-  public selectedTimescale: string = '';
+  public selectedTimescale: string = 'month';
   public timescales = [
     { value: 'hour', label: 'Hour' },
     { value: 'day', label: 'Day' },
