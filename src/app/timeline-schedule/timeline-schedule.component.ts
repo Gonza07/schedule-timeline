b4f913e7b55b611495593978fb31ac804f5f4989
Gonzalo Laborde <gonzalolaborde07@gmail.com>
2025-12-30 21:14:23 -0300

    Add change timescale functionallity

--- src/app/timeline-schedule/timeline-schedule.component.ts
+++ src/app/timeline-schedule/timeline-schedule.component.ts
@@ -1,6 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import dayjs from 'dayjs';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+
+dayjs.extend(weekOfYear);
 
 @Component({
   selector: 'app-timeline-schedule',
@@ -9,8 +18,9 @@ import dayjs from 'dayjs';
   templateUrl: './timeline-schedule.component.html',
   styleUrl: './timeline-schedule.component.scss',
 })
-export class TimelineScheduleComponent implements OnInit {
+export class TimelineScheduleComponent implements OnInit, OnChanges {
   @Input() timeScale: 'hour' | 'day' | 'week' | 'month' = 'day';
+  public timeSlots: dayjs.Dayjs[] = [];
   public days: dayjs.Dayjs[] = [];
   public today = dayjs().startOf('day');
   public timelineStart = this.today.subtract(14, 'day');
@@ -30,6 +40,73 @@ export class TimelineScheduleComponent implements OnInit {
     while (current.isBefore(this.timelineEnd)) {
       this.days.push(current);
       current = current.add(1, 'day');
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['timeScale']) {
+      this.generateTimeSlots();
+    }
+  }
+
+  private generateTimeSlots() {
+    this.timeSlots = [];
+
+    const today = dayjs().startOf('day');
+
+    let start: dayjs.Dayjs;
+    let end: dayjs.Dayjs;
+    let unit: dayjs.ManipulateType;
+
+    switch (this.timeScale) {
+      case 'hour':
+        start = today.subtract(24, 'hour');
+        end = today.add(24, 'hour');
+        unit = 'hour';
+        break;
+
+      case 'week':
+        start = today.subtract(8, 'week').startOf('week');
+        end = today.add(8, 'week').endOf('week');
+        unit = 'week';
+        break;
+
+      case 'month':
+        start = today.subtract(6, 'month').startOf('month');
+        end = today.add(6, 'month').endOf('month');
+        unit = 'month';
+        break;
+
+      case 'day':
+      default:
+        start = today.subtract(14, 'day');
+        end = today.add(14, 'day');
+        unit = 'day';
+        break;
+    }
+
+    let current = start;
+
+    while (current.isBefore(end)) {
+      this.timeSlots.push(current);
+      current = current.add(1, unit);
+    }
+  }
+
+  public formatHeader(slot: dayjs.Dayjs) {
+    switch (this.timeScale) {
+      case 'hour':
+        return slot.format('HH:mm');
+
+      case 'week':
+        return `W${slot.week()}`;
+
+      case 'month':
+        return slot.format('MMM YYYY');
+
+      case 'day':
+      default:
+        return slot.format('DD MMM');
     }
   }
 }
